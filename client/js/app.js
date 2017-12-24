9732fa7dc2a39da2bf48f8b09fc6daa1eee00f16
Scarlett <coffmansr906@gmail.com>
2017-12-24 15:06:27 -0500

    edited songs and got buttons to work

--- client/js/app.js
+++ client/js/app.js
@@ -1,13 +1,81 @@
-(function(){
-    console.log('test');
+var music;
+$('img').click(function(){
 
-    var baseUrl = "./Mary_music/";
-    var audio = ["rudolph.mp3", "merry_christmas.mp3", "mom_nanita.mp3", "dad_joy.mp3", "scarlett_burrito.mp3", 
-    'greg', "deck.mp3", "mary_silent.mp3", "beep-08b.mp3", "beep-09.mp3"];
-    
-    $('img').click(function() {
-        console.log($(this))
-        var i = $(this).attr('id').substring(1);
-        new Audio(baseUrl + audio[i-1]).play();
-    });    
-})();
+    var musicId = $(this).attr('id').substring(1);
+    if(typeof music == "undefined"){
+    music = document.getElementById(musicId);
+    music.currentTime = 0
+    music.play();           
+    } else{
+        var musicP = music;
+        music = document.getElementById(musicId);
+        if((music == musicP) && music.paused){
+            music.currentTime = 0
+            music.play();
+        } else if(music == musicP) {
+            music.pause();
+        }else{
+        musicP.pause();
+        music.currentTime = 0
+        music.play();        
+        }
+    }
+})
+// $(document).ready(function(){
+//     if(music.paused){
+//         $(this).addClass('activate');
+//     } else {
+//         $('i').click(function(){
+//             music.pause();
+//             $(this).addClass('activate');
+//         })     
+//     }
+// }
+
+    // if (img.find('on').length > 0) {
+    //   music = document.getElementById(img.find('on').attr('id'))
+    // } else {
+    // }
+
+    // if($('img.on').length){
+    //     music.pause();
+    //     $(this).find('.on').removeClass('on').addClass('off');
+    //     music = document.getElementById(img.find('on').attr('id'));
+    //     music.play();
+    //     } else{
+    //     music = document.getElementById(musicId);
+    //     music.play();
+    //     $(this).closest('.off').removeClass('off').addClass('on');
+    //     };
+//});
+// (function(){
+//     var baseUrl = "./Mary_music/";
+//     var audio = ["rudolph.mp3", "merry_christmas.mp3", "mom_nanita.mp3", "dad_joy.mp3", "scarlett_burrito.mp3", 
+//     'greg', "deck.mp3", "mary_silent.mp3"];
+   
+//     $('img').click(function() {
+//         var i = $(this).attr('id').substring(1);
+//         var music = new Audio(baseUrl + audio[i-1]);
+//         console.log(music)
+//         if(music.paused){
+//             console.log(music);            
+//             music.play();       
+//         }else{
+//             console.log(music+"2");            
+//             music.pause();
+//         }
+//         // if($('img.on').length){
+//         // console.log(music);
+//         // music.pause();
+//         // $(this).find('.on').removeClass('on').addClass('off');
+//         // }else{     
+//         // console.log($(this));       
+//         // var i = $(this).attr('id').substring(1);
+//         // console.log(i);
+//         // var music = new Audio(baseUrl + audio[i-1]);
+//         // music.play();
+//         // $(this).closest('.off').removeClass('off').addClass('on');
+//         // console.log(music);
+//         // }
+//     });    
+// })();
